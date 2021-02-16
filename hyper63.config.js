fcdfc64f48ac6411ad45f623af0db0c3248b9b34
Tom Wilson <twilson63@gmail.com>
2021-02-16 13:47:32 -0500

    added minio

--- hyper63.config.js
+++ hyper63.config.js
@@ -2,10 +2,13 @@ import express from '@hyper63/app-express'
 import couchdb from '@hyper63/adapter-couchdb'
 import redis from '@hyper63/adapter-redis'
 import es from '@hyper63/adapter-elasticsearch'
+import minio from '@hyper63/adapter-minio'
+
 
 const COUCH = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.COUCHDB_HOST}:5984`
 const REDIS = `redis://${process.env.REDIS_HOST}`
 const ES = `http://${process.env.SEARCH_HOST}:9200`
+const MINIO = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.MINIO}:9000`
 
 export default {
   app: express,
@@ -13,5 +16,6 @@ export default {
     { port: 'data', plugins: [couchdb({url: COUCH })]},
     { port: 'cache', plugins: [redis({url: REDIS})]},
     { port: 'search', plugins: [es({url: ES})]},
+    { port: 'storage', plugins: [minio({url: MINIO})]}
   ]
 }

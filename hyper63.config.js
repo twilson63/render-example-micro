6b10c79b1979d3dc8a795271bc5d87b647f9ada0
Tom Wilson <twilson63@gmail.com>
2021-02-16 13:36:50 -0500

    fix port assignment issues

--- hyper63.config.js
+++ hyper63.config.js
@@ -3,9 +3,9 @@ import couchdb from '@hyper63/adapter-couchdb'
 import redis from '@hyper63/adapter-redis'
 import es from '@hyper63/adapter-elasticsearch'
 
-const COUCH = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.COUCHDB_HOST}`
+const COUCH = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.COUCHDB_HOST}:5984`
 const REDIS = `redis://${process.env.REDIS_HOST}`
-const ES = `http://${process.env.SEARCH_HOST}`
+const ES = `http://${process.env.SEARCH_HOST}:9200`
 
 export default {
   app: express,

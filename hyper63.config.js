e3ee32ace1699ac383bae96a4003911d8f1e6b25
Tom Wilson <twilson63@gmail.com>
2021-02-16 12:20:59 -0500

    add cache

--- hyper63.config.js
+++ hyper63.config.js
@@ -1,11 +1,14 @@
 import express from '@hyper63/app-express'
 import couchdb from '@hyper63/adapter-couchdb'
+import redis from '@hyper63/adapter-redis'
 
 const COUCH = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.COUCHDB_HOST}`
+const REDIS = `redis://${process.env.REDIS_HOST}`
 
 export default {
   app: express,
   adapters: [
-    { port: 'data', plugins: [couchdb({url: COUCH })]}
+    { port: 'data', plugins: [couchdb({url: COUCH })]},
+    { port: 'cache', plugins: [redis({url: REDIS})]}
   ]
 }

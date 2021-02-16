a29e3034c4a5a9e2489ef17953cd26790ca15d25
Tom Wilson <twilson63@gmail.com>
2021-02-16 13:51:28 -0500

    fixed typo in config

--- hyper63.config.js
+++ hyper63.config.js
@@ -8,7 +8,7 @@ import minio from '@hyper63/adapter-minio'
 const COUCH = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.COUCHDB_HOST}:5984`
 const REDIS = `redis://${process.env.REDIS_HOST}`
 const ES = `http://${process.env.SEARCH_HOST}:9200`
-const MINIO = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.MINIO}:9000`
+const MINIO = `http://${process.env.USER}:${process.env.PASSWORD}@${process.env.MINIO_HOST}:9000`
 
 export default {
   app: express,

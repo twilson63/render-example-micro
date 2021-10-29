4b98462c9b7dc95e0ece2f1ac09b2f7c5fc752e8
Tom Wilson <twilson63@gmail.com>
2021-10-29 12:02:17 +0000

    extracted and fixed couchdb adapter

---
+++ couchdb-adapter/async-fetch.js
@@ -0,0 +1,16 @@
+const { Async } = require('crocks')
+const { ifElse, propEq } = require('ramda')
+const { composeK } = require('crocks/helpers')
+
+exports.asyncFetch = Async.fromPromise(fetch)
+exports.createHeaders = (username, password) => ({
+  'Content-Type': 'application/json',
+  authorization: `Basic ${Buffer.from(username + ':' + password).toString('base64')}`
+})
+const toJSON = (result) => Async.fromPromise(result.json.bind(result))();
+const toJSONReject = composeK(Async.Rejected, toJSON);
+
+exports.handleResponse = (code) =>
+  ifElse(propEq("status", code), toJSON, toJSONReject);
+
+
